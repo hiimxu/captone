3649e5bd13b37151f78d12c3f704d3b539007691
HieuDD <mhoguty0213@gmail.com>
2022-03-03 14:36:08 +0700

    create salon owner dashboard

--- src/components/salonOwners/RegisterSalon.js
+++ src/components/salonOwners/RegisterSalon.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import bgImg from "../../assets/introbg-1.jpg";
 
 export default function RegisterSalon() {
+  // const newSalon = {
+  //     account_name: username,
+  //     password: password,
+  //     email: email,
+  //     role: "salon",
+  //     nameSalon: nameSalon,
+  //     phone:phone,
+  //     Passibility:1,
+  //     TaxCode: taxCode
+  // }
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [nameSalon, setNameSalon] = useState("");
+  const [phone, setPhone] = useState("");
+  const [taxCode, setTaxCode] = useState("");
+
+  const root = {
+    textAlign: "center",
+    backgroundImage: `url(${bgImg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };
+
   return (
-    <div>RegisterSalon</div>
-  )
+    <div style={root}>
+      <h2>Become a partner of our program</h2>
+      <div></div>
+    </div>
+  );
 }
