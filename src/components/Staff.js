c0948bc1d1c6e654cbf259d322c815fc07647e85
HieuDD <mhoguty0213@gmail.com>
2022-03-14 22:46:18 +0700

    create component Staff.js

---
+++ src/components/Staff.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { api } from "../api/api.js";
+import { Link } from "react-router-dom";
+
+export default function Staff() {
+    const [staff, setStaff]=useState([])
+    useEffect(() => {
+        fetch(`${api}api/customer/get/staff/1`)
+          .then((res) => res.json())
+          .then((staff) => {
+            setStaff(staff);
+            console.log(staff);
+          });
+      }, []);
+  return (
+    <div className="p-5 ">
+      <div className="card-group">
+        {staff.data?.map((staff) => (
+          <div className="card m-2 rounded" style={{ width: "18rem" }}>
+            <div className="" key={staff.staffId}>
+              {/* <img className="card-img-top rounded-top mb-2 border-0" src={salon.image} alt="Salon image" style={{width:"100%",height:"20rem",objectFit: 'cover', objectPosition:"top"}} /> */}
+              <div className="card-body">
+              <h5 className="card-title">{staff.name}</h5>
+              <p className="card-text">Phone: {staff.phone}</p>              
+              <Link to="/services" className="btn btn-primary">Book now</Link>
+              </div>
+            </div>
+          </div>
+          
+        ))}
+      </div>
+    </div>
+  )
+}
