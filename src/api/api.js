787753da07c611347624dbd64a89054e8554889c
HieuDD <mhoguty0213@gmail.com>
2022-04-14 16:02:41 +0700

    config connect api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
-// export const api = "http://localhost:8080/";
-export const api = "https://26c6-117-7-187-179.ngrok.io/";
+export const api = "http://localhost:8080/";
+// export const api = "https://26c6-117-7-187-179.ngrok.io/";
